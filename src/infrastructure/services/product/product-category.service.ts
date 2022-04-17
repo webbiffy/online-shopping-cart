0b099ff188df34aa6d25897a40bf1b9e153e83e3
Webster <webster.pacheco@gmail.com>
2022-04-17 17:01:27 +0800

    to be continue

--- src/infrastructure/services/product/product-category.service.ts
+++ src/infrastructure/services/product/product-category.service.ts
@@ -4,6 +4,6 @@ import { CategoryItem } from "../../../application/models/product/category-item"
 // todo: use api handler instead
 export default {
     async getProductCategoryItems (): Promise<CategoryItem[]> {
-        return await (await fetch('https://my-json-server.typicode.com/webbiffy/online-shopping-cart/categories')).json()
+        return await (await fetch('https://my-json-server.typicode.com/webbiffy/shopping-cart.github.io/categories')).json()
     }
 }
